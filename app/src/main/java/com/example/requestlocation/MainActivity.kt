64124627733890d88904c312a3ad1024524086f9
CHERFAOUI Oussama <64999736+oussama-dz@users.noreply.github.com>
2023-06-18 16:29:31 +0100

    update

--- app/src/main/java/com/example/requestlocation/MainActivity.kt
+++ app/src/main/java/com/example/requestlocation/MainActivity.kt
@@ -5,41 +5,37 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.example.requestlocation.ui.LocationService
-import kotlinx.coroutines.launch
+import com.mapbox.geojson.Point
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GetCurrentLocationUI()
+            MapScreen()
         }
     }
 }
 
 @Composable
-fun GetCurrentLocationUI() {
+fun MapScreen() {
 
-    var currentLocation by remember {
-        mutableStateOf("")
+    var point: Point? by remember {
+        mutableStateOf(null)
     }
-
-    val scope = rememberCoroutineScope()
+    var relaunch by remember {
+        mutableStateOf(false)
+    }
 
     val context = LocalContext.current
 
@@ -50,54 +46,52 @@ fun GetCurrentLocationUI() {
             if (!permissions.values.all { it }) {
                 //handle permission denied
             }
+            else {
+                relaunch = !relaunch
+            }
         }
     )
 
 
     Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically)
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Button(
-            onClick = {
-                scope.launch {
-                    try {
-                        val location = LocationService().getCurrentLocation(context)
-                        currentLocation =
-                            "Latitude: ${location.latitude}, Longitude: ${location.longitude}"
+        MapBoxMap(
+            onPointChange = { point = it },
+            point = point,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+    }
 
-                    } catch (e: LocationService.LocationServiceException) {
-                        when (e) {
-                            is LocationService.LocationServiceException.LocationDisabledException -> {
-                                //handle location disabled, show dialog or a snack-bar to enable location
-                            }
+    LaunchedEffect(key1 = relaunch) {
+        try {
+            val location = LocationService().getCurrentLocation(context)
+            point = Point.fromLngLat(location.longitude, location.latitude)
 
-                            is LocationService.LocationServiceException.MissingPermissionException -> {
-                                permissionRequest.launch(
-                                    arrayOf(
-                                        android.Manifest.permission.ACCESS_FINE_LOCATION,
-                                        android.Manifest.permission.ACCESS_COARSE_LOCATION
-                                    )
-                                )
-                            }
+        } catch (e: LocationService.LocationServiceException) {
+            when (e) {
+                is LocationService.LocationServiceException.LocationDisabledException -> {
+                    //handle location disabled, show dialog or a snack-bar to enable location
+                }
 
-                            is LocationService.LocationServiceException.NoNetworkEnabledException -> {
-                                //handle no network enabled, show dialog or a snack-bar to enable network
-                            }
+                is LocationService.LocationServiceException.MissingPermissionException -> {
+                    permissionRequest.launch(
+                        arrayOf(
+                            android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION
+                        )
+                    )
+                }
 
-                            is LocationService.LocationServiceException.UnknownException -> {
-                                //handle unknown exception
-                            }
-                        }
-                    }
+                is LocationService.LocationServiceException.NoNetworkEnabledException -> {
+                    //handle no network enabled, show dialog or a snack-bar to enable network
+                }
+
+                is LocationService.LocationServiceException.UnknownException -> {
+                    //handle unknown exception
                 }
             }
-        ) {
-            Text(text = "Get Current Location")
         }
-
-        Text(text = currentLocation)
     }
 }
